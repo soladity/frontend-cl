ef4a34ba51da72236ef9b205d6fa62a728001a08
soladity <thomasfullstack@gmail.com>
2022-02-02 12:50:17 -0800

    Dashboard
    Home - Dashboard - Top Part

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -2,53 +2,174 @@ import React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import CardsComponent from '../../component/Cards/Cards';
-import { Grid } from '@mui/material';
-import CardSm from '../../component/Cards/CardSm';
+import { Grid, Card, Box, Button, Popover } from '@mui/material';
 import { meta_constant } from '../../config/meta.config';
 import Helmet from 'react-helmet';
+import { makeStyles } from '@mui/styles';
+import { getTranslation } from '../../utils/translation';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '180px',
+        height: '100%'
+    },
+});
 
 const Home = () => {
+
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+    const handleClickCreateLegion = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleCloseCreateLegion = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'create-legion-popover' : undefined;
+
     return (
-        <div>
+        <Box>
             <Helmet>
                 <meta charSet="utf-8" />
-                <title>{meta_constant.home.title}</title>
-                <meta name="description" content={meta_constant.home.description} />
-                { meta_constant.home.keywords && <meta name="keywords" content={meta_constant.home.keywords.join(',')} /> }
+                <title>{meta_constant.beasts.title}</title>
+                <meta name="description" content={meta_constant.beasts.description} />
+                {meta_constant.beasts.keywords && <meta name="keywords" content={meta_constant.beasts.keywords.join(',')} />}
             </Helmet>
-            <Typography variant="h3">CryptoLegions Home</Typography>
-            <Typography variant="h5">Game of Crypto</Typography>
-            <Typography variant="body1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-            </Typography>
-            <br />
+            <Grid container spacing={2} sx={{ my: 4 }}>
+                <Grid item xs={12} md={4}>
+                    <Card className={classes.card}>
+                        <Box sx={{ p: 4, justifyContent: 'center' }}>
+                            <Typography variant='h6' sx={{ fontWeight: 'bold', textAlign: 'center', borderBottom: '1px solid #fff', marginBottom: 3 }}>
+                                NADODO IS WATCHING
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                MARKETPLACE TAX : 15%
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                HUNT TAX : 2%
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                # LEGIONS : 8 / 12
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                YOUR MAX: 60000
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                UNCALIMED $ : 558
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                TAX DAYS LEFT : 3
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card className={classes.card}>
+                        <Box sx={{ p: 4, justifyContent: 'center' }}>
+                            <Typography variant='h6' sx={{ fontWeight: 'bold', textAlign: 'center', borderBottom: '1px solid #fff', marginBottom: 3 }}>
+                                YOUR LEGIONS:
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                # BEASTS : 3
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                # WARRORS : 5
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                # LEGIONS : 8 / 12
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                YOUR MAX: 60000
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                UNCALIMED $ : 558
+                            </Typography>
+                            <Typography variant='subtitle1' color='primary' sx={{ fontWeight: 'bold' }}>
+                                TAX DAYS LEFT : 3
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Card className={classes.card}>
+                        <Box sx={{ p: 4, justifyContent: 'center' }}>
+                            <Typography variant='h6' sx={{ fontWeight: 'bold', textAlign: 'center', borderBottom: '1px solid #fff', marginBottom: 3 }}>
+                                TAKE ACTION
+                            </Typography>
+                            <div style={{ textAlign: 'center', marginBottom: 8 }}>
+                                <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                    SUMMON BEAST
+                                </Button>
+                            </div>
+                            <div style={{ textAlign: 'center', marginBottom: 8 }}>
+                                <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                    SUMMON WARROIR
+                                </Button>
+                            </div>
+                            <div style={{ textAlign: 'center', marginBottom: 8 }}>
 
-            <Grid container spacing={2} sx={{mb: 4}}>
-                <Grid item xs={3}>
-                    <CardSm image={"https://uc73451e395a40e1bbf333aa1dce.previews.dropboxusercontent.com/p/thumb/ABbcaRgSyKClSK8DTWg77StVIlUtauzezyQumBVvchP60a-XaXMq9CU1J2DjuIcCt3DHAERYdXOA7SY_QRrj1BqVtQaUwnDYfFfCPyLbDYxVvS0dcRSgwQ8Uj9TIFYd2Cb_OUh61HGQpWLcgExNfZXXmotJvqRsFLxywkiUYtCVRoAZoPh8meIlIMVKijqssGdh_5JNN_iyNUipqpSVChO2G8836-C8yve1l1fQKbM0Z3MJ7ePAOJS-CzZYfa-GigEEaWDZFUmvLT4bi8vx-zBjhLsb3qhomXv0Tb6VAqqdvndowaEI6-W7L_r1RXv8Nlh2yK27Z1dmki18lO0LQ2QhPgGgEivaX8OjUFRj00P_1nWFtvK67oB8J2kmxZOQkg8U/p.gif"} />
-                </Grid>
-                <Grid item xs={3}>
-                    <CardSm image={"https://ucb2b30381f664573476b8d8e685.previews.dropboxusercontent.com/p/thumb/ABYJSojYwEXlllWqDPOZvsFb4F2ENjt-1ecROewf1hYrWZ8G8RviWzVVEZD0P5sQTtBHeQ4yF9MzaKqxxD5iECkOOYVcig6hy3atskB4GoZdoQzpXtm7JsUYllJuqnv_pQe1p0P7PtnJGOgiWMRWrRj0sYCRVV0S-0htBep-x3P9DDFc32Pip8520vrmacEJiqk_Py7wJZbrJ6IJlwqwItr1epmsyW1fUcA83XHTeBVhxA7qzG8bTqRv6WZtwBZUlWE-IqNByg6f295Jus7SmQzboX8o91ljwwtyahjFmHj61DdJn1r6fGuN81dthdTJWMRLxKaZHPl9VoIwxFOCoIKduYyhWlG33B6fp7LFKqM27LNk98yH_IDpv89wkIo99k4/p.gif"} />
-                </Grid>
-                <Grid item xs={3}>
-                    <CardSm image={"https://uc84480d507953c4b2bf27c4182e.previews.dropboxusercontent.com/p/thumb/ABbWehB6_2oyMu23daGpRMBnJPv6yQk7QKLEQJwq5w62RpBKy3wJTQJzFpvtcaGGFG-rr0YfJKAO_fmho3ne2eDH610thfynmcQOaH9cOF51I8TSumUD2umqAcsTBNwgSTW556m6M4cQBo1Ha4fObqrlsoeaNdLRDzCU6x--73OSNehddTugv2_MPG9SJy_lRtL3sHq1MOReaRTI86SWXedI0gl09AiL_LeDnJei8cq49FHBwc3qeG1GfWH3CKZFsvNeC5gRr1JsGiLLaBEWCHsDbiSDI48YWVxcBwD-IYWA27OtqOzdon5mJv-vTbls_vYchphdExHfTtCHcqRjgvUq73eR3Z9YmlvYV0g_YPsGKBxzakaO4g3lBf1jQsgeWaCKJ4c7lWDNZ0gMzKP7R5yM7KGSerkW2HfshaYz7Wp1hgGD9EfxOk840utFTun-hdU/p.gif"} />
-                </Grid>
-                <Grid item xs={3}>
-                    <CardSm image={"https://uc451be996496db762bb7322ab38.previews.dropboxusercontent.com/p/thumb/ABZL5FFsGyRtlEoXhuk1f8Fr8Dy1Oc6q3bCdGcsr57hx6SiL_oM_WNIPltDzgmPuIfDGMyKLoYPEMQDSww9sRhxhodccli1KnUODSpx8hK-zM8cxoPWN-zx7RYhGVj3KReqQdbYiaTnTZGYzP0MIGyO7YsFBkbg1Spun-_0pF5xwGGaxjapXsfQCe7Y4EEdpCoRQIF6uztXRistOpwNmO7fin1M7nbHfB3Ji9jOruc3r8fQ7TgeMiljy6GfXxy6EUf-2xHZHUOPrYOZhpyIMiuIknowzHgoWq_yHo3jUZcwA4f-4hueXVgy3mOAKyGdVUh98tN3zKFoU6F63spHdeqkx2gr5LWRYnIOKujLPbQO38LqYSXl-YZuDjuOKQ7fTValMNXEPrwSa4xczK-vemGDfbOU8ar28FsldyX8yguNLj0Z5CCj7bo7z53ntG5Qfi7jS1wUDr7XpqazTYSmvJv5l1L2guk2L1OOY8Ru0sH3xbDNyUi78ghLyeQJ2-kUJVV8/p.gif"} />
+                                <Button aria-describedby={id} variant="contained" onClick={handleClickCreateLegion} sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                    CREATE LEGION
+                                </Button>
+                                <Popover
+                                    id={id}
+                                    open={open}
+                                    anchorEl={anchorEl}
+                                    onClose={handleCloseCreateLegion}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'center',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'center',
+                                    }}
+                                    style={{ marginTop: 6, padding: 10 }}
+                                >
+                                    <Box sx={{ p: 2 }}>
+                                        <div style={{ textAlign: 'center', marginBottom: 8 }}>
+                                            <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                                BUY BEASTS
+                                            </Button>
+                                        </div>
+                                        <div style={{ textAlign: 'center', marginBottom: 8 }}>
+                                            <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                                BUY WARRIORS
+                                            </Button>
+                                        </div>
+                                        <div style={{ textAlign: 'center', marginBottom: 8 }}>
+                                            <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                                BUY LEGIONS
+                                            </Button>
+                                        </div>
+                                        <div style={{ textAlign: 'center' }}>
+                                            <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                                BUY $BLST
+                                            </Button>
+                                        </div>
+                                    </Box>
+                                </Popover>
+                            </div>
+                            <div style={{ textAlign: 'center' }}>
+                                <Button variant="contained" sx={{ fontWeight: 'bold', fontSize: 12 }}>
+                                    HUNT
+                                </Button>
+                            </div>
+                        </Box>
+                    </Card>
                 </Grid>
             </Grid>
-        </div>
+        </Box >
     )
 }
 
