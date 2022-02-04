0da87957b88ee69bf94f5c8cabb205586c548150
soladity <thomasfullstack@gmail.com>
2022-02-04 01:43:37 -0800

    summon successfully with SnackBar
    summon successfully with SnackBar

--- src/containers/Home/TakeAction.tsx
+++ src/containers/Home/TakeAction.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
-import { Grid, Card, Box, Button, Popover, Checkbox, Dialog, DialogTitle } from '@mui/material';
+import { Grid, Card, Box, Button, Popover, Checkbox, Dialog, DialogTitle, Snackbar, Alert } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { FaTimes } from 'react-icons/fa'
 import { getBeastBloodstoneAllowance, setBeastBloodstoneApprove, getWarriorBloodstoneAllowance, setWarriorBloodstoneApprove, mintBeast, mintWarrior } from '../../hooks/contractFunction';
 import { useWeb3React } from '@web3-react/core';
 import { useBloodstone, useBeast, useWarrior, useWeb3 } from '../../hooks/useContract';
 import { useNavigate } from 'react-router-dom'
+import Slide, { SlideProps } from '@mui/material/Slide';
+
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
+
+function TransitionUp(props: TransitionProps) {
+    return <Slide {...props} direction="up" />;
+}
 
 const TakeAction = () => {
     const navigate = useNavigate()
@@ -41,28 +49,43 @@ const TakeAction = () => {
     const web3 = useWeb3();
 
     //Mint Beast with quantity
-    const handleBeastMint = async (amount: Number) => {
+    const handleBeastMint = async (amount: Number, Transition: React.ComponentType<TransitionProps>) => {
         handlePopoverCloseSummonBeast()
         const allowance = await getBeastBloodstoneAllowance(web3, bloodstoneContract, account);
         console.log('allowance --- ', allowance)
         if (allowance === '0') {
             await setBeastBloodstoneApprove(web3, bloodstoneContract, account);
         }
-        const result = await mintBeast(web3, beastContract, account, amount);
-        console.log(result)
-        navigate('/beasts')
+        await mintBeast(web3, beastContract, account, amount);
+
+        setTransition(() => Transition);
+        setSnackBarMessage('Summon Beasts successfully! If you see your all beasts, please click this!')
+        setSnackBarNavigation('/beasts')
+        setOpenSnackBar(true)
     }
 
     //Mint Warriors with quantity
-    const handleWarriorMint = async (amount: Number) => {
+    const handleWarriorMint = async (amount: Number, Transition: React.ComponentType<TransitionProps>) => {
         handlePopoverCloseSummonWarrior()
         const allowance = await getWarriorBloodstoneAllowance(web3, bloodstoneContract, account);
         if (allowance === '0') {
             await setWarriorBloodstoneApprove(web3, bloodstoneContract, account);
         }
         await mintWarrior(web3, warriorContract, account, amount);
-        navigate('/beasts')
+
+        setTransition(() => Transition);
+        setSnackBarMessage('Summon Warriors successfully! If you see your all warriors, please click this!')
+        setSnackBarNavigation('/warriors')
+        setOpenSnackBar(true)
     }
+
+    //SnackBar
+    const [openSnackBar, setOpenSnackBar] = React.useState(false)
+    const [snackBarMessage, setSnackBarMessage] = React.useState('')
+    const [snackBarNavigation, setSnackBarNavigation] = React.useState('')
+    const [transition, setTransition] = React.useState<
+        React.ComponentType<TransitionProps> | undefined
+    >(undefined);
 
     return (
         <Card sx={{
@@ -71,6 +94,20 @@ const TakeAction = () => {
             minHeight: '180px',
             height: '100%'
         }}>
+            <Snackbar
+                open={openSnackBar}
+                TransitionComponent={transition}
+                autoHideDuration={6000}
+                onClose={() => setOpenSnackBar(false)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                key={transition ? transition.name : ''}
+            >
+                <Alert onClose={() => setOpenSnackBar(false)} variant='filled' severity="success" sx={{ width: '100%' }}>
+                    <Box sx={{ cursor: 'pointer' }} onClick={() => navigate(snackBarNavigation)}>
+                        {snackBarMessage}
+                    </Box>
+                </Alert>
+            </Snackbar>
             <Box sx={{ p: 4, justifyContent: 'center' }}>
                 <Typography variant='h6' sx={{ fontWeight: 'bold', textAlign: 'center', borderBottom: '1px solid #fff', marginBottom: 3 }}>
                     TAKE ACTION
@@ -102,10 +139,10 @@ const TakeAction = () => {
                                         </Box>
                                         <DialogTitle>SUMMON BEAST</DialogTitle>
                                         <Box sx={{ padding: 3, display: 'flex', flexDirection: 'column' }}>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(1)}>1</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(5)}>5</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(10)}>10</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(20)}>20</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(1, TransitionUp)}>1</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(5, TransitionUp)}>5</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(10, TransitionUp)}>10</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleBeastMint(20, TransitionUp)}>20</Button>
                                             <Button variant='contained' sx={{ fontWeight: 'bold' }}>100</Button>
                                         </Box>
                                     </Popover>
@@ -131,10 +168,10 @@ const TakeAction = () => {
                                         </Box>
                                         <DialogTitle>SUMMON WARRIORS</DialogTitle>
                                         <Box sx={{ padding: 3, display: 'flex', flexDirection: 'column' }}>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(1)}>1</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(5)}>5</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(10)}>10</Button>
-                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(20)}>20</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(1, TransitionUp)}>1</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(5, TransitionUp)}>5</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(10, TransitionUp)}>10</Button>
+                                            <Button variant='contained' sx={{ marginBottom: 1, fontWeight: 'bold' }} onClick={() => handleWarriorMint(20, TransitionUp)}>20</Button>
                                             <Button variant='contained' sx={{ fontWeight: 'bold' }}>100</Button>
                                         </Box>
                                     </Popover>
