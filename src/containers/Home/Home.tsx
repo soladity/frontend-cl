34c8f3fee2f90149090eb6c59dc83ac28abe1f8f
soladity <thomasfullstack@gmail.com>
2022-02-13 05:40:23 -0800

    update home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -62,15 +62,15 @@ const Home = () => {
                         <Grid item md={3} sm={2} xs={1}>
                         </Grid>
                         <Grid item md={6} sm={8} xs={10}>
-                            {/* <YouTube
-                                videoId="SA-PmNW7syw"
-                                opts={{ width: '100%', height: '100%' }}
-                            /> */}
-                            <ReactPlayer
-                                url="https://youtu.be/SA-PmNW7syw"
-                                width={"100%"}
-                                height="100%"
-                            />
+                            <Box sx={{ width: '100%', paddingTop: '75%', position: 'relative' }}>
+                                <Box sx={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }}>
+                                    <ReactPlayer
+                                        url="https://youtu.be/SA-PmNW7syw"
+                                        width="100%"
+                                        height="100%"
+                                    />
+                                </Box>
+                            </Box>
                         </Grid>
                         <Grid item md={3} sm={2} xs={1}>
                         </Grid>
