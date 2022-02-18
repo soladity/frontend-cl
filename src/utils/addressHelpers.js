2cfef8b68c5f3c58e16e0d46a2576a0fa2ce0cc9
soladity <thomasfullstack@gmail.com>
2022-02-18 13:22:32 -0700

    update

--- src/utils/addressHelpers.js
+++ src/utils/addressHelpers.js
@@ -3,25 +3,25 @@ export const getBloodstoneAddress = () => {
 };
 
 export const getBeastAddress = () => {
-  return "0x22ec77a895c5c6e9dcb7dd6b06f6cf5d364257cf";
+  return "0x1edbbbb0430729f2744c7e4a63bb2ea31a5b74f4";
 };
 
 export const getWarriorAddress = () => {
-  return "0xe05402c2c8a741360c542444f43721af5a63aa49";
+  return "0x27cd18b2ed649316cfa98cee8478783376de2c87";
 };
 
 export const getLegionAddress = () => {
-  return "0x97BBF34109875FEe6dB01b055d64dFe7d32EA4C4";
+  return "0x02D2BD0f35F2C9061DE829028121fc82d9210434";
 };
 
 export const getMonsterAddress = () => {
-  return "0xa0894ec2fed3a84ae497314aa2cbeb38c1ce6625";
+  return "0x01d2215ed81d60f6c2d41320b0221b120c72ef57";
 };
 
 export const getRewardPoolAddress = () => {
-  return "0x9175a4be8f938e71d3418a07617d3c144772f981";
+  return "0xb5cb74ceedc6b39eee96f67364f4e9ad0a66e2d1";
 };
 
 export const getMarketplaceAddress = () => {
-  return "0xe02f19963b4303c2dc0f8d6d0b73365e9631225d";
+  return "0x536a38b4b9caab93c4bed7ec8400f33c3a319489";
 };
