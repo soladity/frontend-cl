a2ece6139a09733862ab635335aaa90e07b02dc2
soladity <thomasfullstack@gmail.com>
2022-03-05 07:22:45 +0900

    fix: update contract addresses

--- src/utils/addressHelpers.js
+++ src/utils/addressHelpers.js
@@ -3,29 +3,29 @@ export const getBloodstoneAddress = () => {
 };
 
 export const getBeastAddress = () => {
-  return "0xb878bd666f5dbd7e7b2d446f713867656ca0bb69";
+  return "0xf920fa95aae0aa29f21c8d0fb81a173029e2e667";
 };
 
 export const getWarriorAddress = () => {
-  return "0x7857e61e16e4d870ef2b6708fd42fc060978c2ed";
+  return "0xba613deabc92ca8c96b1037b2134f566ef013688";
 };
 
 export const getLegionAddress = () => {
-  return "0x023a368a269C60fb7701b7E20CB3bF46EeD508bc";
+  return "0xc35baB03c4D19AB6e38F07Bf8845831B819e2C12";
 };
 
 export const getMonsterAddress = () => {
-  return "0x536e138d0e3cb01bb9750c9372e6eef160bb7992";
+  return "0x4a9f3d16d34dea6f50520a7b9acf234895ed18df";
 };
 
 export const getRewardPoolAddress = () => {
-  return "0x84fa07f1662056ed9afa3b8019e3db4153760552";
+  return "0x5ec441222e0d387875c3c8300bddd20c15fa4b6a";
 };
 
 export const getMarketplaceAddress = () => {
-  return "0xcb23d3443f20df2d2a293cd6230d557ae5d8f1ce";
+  return "0xb5c123445c5692ff4ab7394660a0f83f282fa19c";
 };
 
 export const getFeeHandlerAddress = () => {
-  return "0xc153d1397ba248dca1fe6eaaa5b0eecfb04516f0";
+  return "0x7b2acc9b419fb46e59d0309e7fb11e707aa6ee19";
 };
