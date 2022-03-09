7e1737d35beaf1529ff60136fc5c8fdb980a7893
soladity <thomasfullstack@gmail.com>
2022-03-08 21:06:15 -0500

    update login page

--- src/component/AskMetaLogin/AskMetaLogin.tsx
+++ src/component/AskMetaLogin/AskMetaLogin.tsx
@@ -100,17 +100,12 @@ const AskMetaLogin = () => {
   };
 
   React.useEffect(() => {
-    console.log('test---------')
-    console.log(error)
-    console.log(process.env.REACT_APP_CHAIN_ID)
-    console.log(process.env.REACT_APP_CHAIN_ID_HEX)
-    console.log('test---------')
     if (error) {
-      if (error.toString().indexOf("NoEthereumProviderError") > -1) {
+      if (error.toString().indexOf("No Ethereum") > -1) {
         setErrorMsg("Please Install MetaMask!");
         setOpenSnackBar(true);
       }
-      if (error.toString().indexOf("UnsupportedChainIdError") > -1) {
+      if (error.toString().indexOf("Unsupported") > -1) {
         switchNetwork();
         setErrorMsg("Please choose Kovan Network!");
         setOpenSnackBar(true);
