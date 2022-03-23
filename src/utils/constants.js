06cb1873bd557c7a04cb150f5db9ce53023736a7
soladity <thomasfullstack@gmail.com>
2022-03-23 16:52:49 -0400

    update new contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,31 +4,31 @@ const address = {
     56: "",
   },
   beast: {
-    42: "0x38575d42f489dc495d0fe695397413635c13f94a",
+    42: "0x2de11c3f7cd05652d7e9d495cf689dc65ee00e9d",
     56: "",
   },
   warrior: {
-    42: "0x893b70089ffc73183487fcf2f1e8a3e966a884ca",
+    42: "0x9c5c6fda71f74b1a980552f84fb112893495d78b",
     56: "",
   },
   legion: {
-    42: "0x5FecA18578Ba1405B0015fD3BEcD812E6ecd93e4",
+    42: "0x8056172AB0381f44896D12FB878A68991AAd861D",
     56: "",
   },
   monster: {
-    42: "0x00bd4c78b5e0c3d83be43b58ad0c4010be171aae",
+    42: "0xcaf9bf1384c4164f3fa6462ad22254ceb1a8ea4d",
     56: "",
   },
   rewardPool: {
-    42: "0x824d9959c510b9291e7c65ce94d4afc7089ebbaf",
+    42: "0x35721a8605e92940ff393d9eeafb2e9a76b6c563",
     56: "",
   },
   marketplace: {
-    42: "0xd6356370c767d4db94f5e1d0851c7b8cfd71a3be",
+    42: "0x1c8dc9f686a8a32582d35c995ff15139784a3bea",
     56: "",
   },
   feeHandler: {
-    42: "0x519a3774510d05f506dad065c4acf54a322185e6",
+    42: "0x2b726f8541e1ccf2ab1322a98927c161a5f2f6b9",
     56: "",
   },
 };
