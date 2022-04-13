076147f4132f4886207806256b8612e4cd368906
soladity <thomasfullstack@gmail.com>
2022-04-12 22:08:21 -0400

    update quote with sparkles

--- src/component/UI/Sparkless/Sparkles.jsx
+++ src/component/UI/Sparkless/Sparkles.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  wrapper: {
+    position: "absolute",
+    pointerEvent: "none",
+    animation: "$growAndShrink 600ms ease-in-out forwards",
+  },
+  svg: {
+    animation: "$spin 600ms linear forwards",
+  },
+  "@keyframes growAndShrink": {
+    "0%": {
+      transform: "scale(0)",
+    },
+    "50%": {
+      transform: "scale(1)",
+    },
+    "100%": {
+      transform: "scale(0)",
+    },
+  },
+  "@keyframes spin ": {
+    "0%": {
+      transform: "scale(0) rotate(0deg)",
+    },
+    "50%": {
+      transform: "scale(1)",
+    },
+    "100%": {
+      transform: "scale(0) rotate(180deg)",
+    },
+  },
+});
 
 const SparkleInstance = ({ color, size, style }) => {
+  const classes = useStyles();
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,31 +43,30 @@ const SparkleInstance = ({ color, size, style }) => {
       height={size}
       fill="none"
       viewBox="0 0 160 160"
-      style={{ position: 'absolute', pointerEvents: 'none', zIndex: 2 }}
+      style={style}
+      className={classes.svg}
     >
       <path
-        fill="#FFC700"
+        fill={color}
         d="M80 0s4.285 41.292 21.496 58.504C118.707 75.715 160 80 160 80s-41.293 4.285-58.504 21.496S80 160 80 160s-4.285-41.293-21.496-58.504C41.292 84.285 0 80 0 80s41.292-4.285 58.504-21.496C75.715 41.292 80 0 80 0z"
       ></path>
     </svg>
   );
-}
-// const Svg = styled.svg`
-//   position: absolute;
-//   pointer-events: none;
-//   z-index: 2;
-// `;
+};
+
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const useRandomInterval = (callback, minDelay, maxDelay) => {
   const timeoutId = React.useRef(null);
   const savedCallback = React.useRef(callback);
+
   React.useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
+
   React.useEffect(() => {
     let isEnabled =
-      typeof minDelay === 'number' && typeof maxDelay === 'number';
+      typeof minDelay === "number" && typeof maxDelay === "number";
     if (isEnabled) {
       const handleTick = () => {
         const nextTickAt = random(minDelay, maxDelay);
@@ -51,7 +85,8 @@ const useRandomInterval = (callback, minDelay, maxDelay) => {
   return cancel;
 };
 
-const DEFAULT_COLOR = 'hsl(50deg, 100%, 50%)';
+const DEFAULT_COLOR = "hsl(50deg, 100%, 50%)";
+
 const generateSparkle = (color = DEFAULT_COLOR) => {
   return {
     id: String(random(10000, 99999)),
@@ -61,69 +96,53 @@ const generateSparkle = (color = DEFAULT_COLOR) => {
     size: random(10, 20),
     style: {
       // Pick a random spot in the available space
-      top: random(0, 100) + '%',
-      left: random(0, 100) + '%',
+      position: "absolute",
+      pointerEvent: "none",
+      top: random(0, 100) + "%",
+      left: random(0, 100) + "%",
       // Float sparkles above sibling content
       zIndex: 2,
     },
-  }
-}
+  };
+};
 
 function Sparkles({ children }) {
-  // const sparkle = generateSparkle();
   const [sparkles, setSparkles] = React.useState([]);
+  const classes = useStyles();
 
-  useRandomInterval(() => {
-    const now = Date.now();
+  useRandomInterval(
+    () => {
+      const now = Date.now();
 
-    const sparkle = generateSparkle();
+      const sparkle = generateSparkle("white");
 
-    const nextSparkles = sparkles.filter(sparkle => {
-      const delta = now - sparkle.createdAt;
-      return delta < 1000;
-    })
+      const nextSparkles = sparkles.filter((sparkle) => {
+        const delta = now - sparkle.createdAt;
+        return delta < 1000;
+      });
 
-    nextSparkles.push(sparkle);
-    setSparkles(nextSparkles)
-  })
+      nextSparkles.push(sparkle);
+      setSparkles(nextSparkles);
+    },
+    50,
+    1000
+  );
 
   return (
-    <div style={{ position: 'relative', display: 'inline-block' }}>
-      {sparkles.map((sparkle, index) => (
+    <div style={{ position: "relative", display: "inline-block" }}>
+      {sparkles.map((sparkle) => (
         <SparkleInstance
-          key={{ index }}
+          key={sparkle.id}
           color={sparkle.color}
           size={sparkle.size}
           style={sparkle.style}
         />
       ))}
-      <div style={{ position: " relative", zIndex: 1, fontWeight: 'bold' }}>
+      <div style={{ position: " relative", zIndex: 1, fontWeight: "bold" }}>
         {children}
       </div>
     </div>
-    // <Wrapper>
-    //   {/* <SparkleInstance
-    //     color={sparkle.color}
-    //     size={sparkle.size}
-    //     style={sparkle.style}
-    //   />
-    //   <ChildWrapper> */}
-    //   {children}
-    //   {/* </ChildWrapper> */}
-    // </Wrapper>
-  )
+  );
 }
-
-
-// const Wrapper = styled.span`
-//   position: relative;
-//   display: inline-block;
-// `;
-
-// const ChildWrapper = styled.strong`
-//   position: relative;
-//   z-index: 1;
-//   font-weight: bold;
-// `;
 
 export default Sparkles;
