c2c8e60afcb19658a3589b9efa20b0d20f08735f
soladity <thomasfullstack@gmail.com>
2022-06-15 16:14:25 -0400

    update tip

--- src/constant/tips/tips.js
+++ src/constant/tips/tips.js
@@ -1,15 +1,15 @@
 export const tipsConstant = {
-	content: [
-		{
-			type: "h5",
-			text: "Legions University",
-		},
+  content: [
+    // {
+    //   type: "h5",
+    //   text: "Legions University",
+    // },
+    // {
+    //   type: "space",
+    // },
     {
-      type: "space",
-    },
-		{
       type: "body1",
-      text: "Every week, here at Legions University, you can attend our live Tips & Tricks sessions on <a class='action' href='https://t.me/CryptoLegionsCommunity'>our Telegram video chat</a>, on Wednesdays at 5pm UTC. During these sessions, we'll be teaching you smart strategies to play the game and maximise your ROI. If you attend live, you will be able to ask your questions. The replays are available on this page. Using all smart strategies together may increase your monthly ROI up to 50%! Make sure to follow all Legions University sessions and apply all tips & tricks together to hunt smartly.",
+      text: "Every week, here at Legions University, you can attend our live Tips & Tricks sessions on <a class='action' href='https://t.me/CryptoLegionsCommunity' target='_blank'>our Telegram video chat</a>, on Wednesdays at 5pm UTC. During these sessions, we'll be teaching you smart strategies to play the game and maximise your ROI. If you attend live, you will be able to ask your questions. The replays are available on this page. Using all smart strategies together may increase your monthly ROI up to 50%! Make sure to follow all Legions University sessions and apply all tips & tricks together to hunt smartly.",
     },
     {
       type: "space",
@@ -17,124 +17,142 @@ export const tipsConstant = {
     {
       type: "space",
     },
-		{
-			type: "h5",
-			text: "Legions University Curriculum",
-		},
     {
-      type: "space",
-    },
-		{
-			type: "subtitle1",
-			text: "Session 1: Summoning (15 June - 5pm UTC)",
-		},
-		{
-			type: "body1",
-			text: "- How to summon smartly?",
-		},
-		{
-			type: "body1",
-			text: "- How to create legions quickly?",
-		},
-		{
-			type: "body1",
-			text: "- How to optimise your Attack Power?",
-		},
-    {
-      type: "space",
-    },
-		{
-			type: "subtitle1",
-			text: "Session 2: Hunting (22 June - 5pm UTC)",
-		},
-		{
-			type: "body1",
-			text: "- Is hunting stronger monsters better?",
-		},
-		{
-			type: "body1",
-			text: "- How to hunt monsters faster?",
-		},
-		{
-			type: "body1",
-			text: "- How to optimise your hunt timing?",
-		},
-    {
-      type: "space",
-    },
-		{
-			type: "subtitle1",
-			text: "Session 3: Claiming (29 June - 5pm UTC)",
-		},
-		{
-			type: "body1",
-			text: "- What is the optimal claiming time?",
-		},
-		{
-			type: "body1",
-			text: "- How to speed up tax decrease?",
-		},
-		{
-			type: "body1",
-			text: "- How does reinvesting earnings impact ROI?",
-		},
-    {
-      type: "space",
-    },
-		{
-			type: "subtitle1",
-			text: "Session 4: Using Unclaimed (6 July - 5pm UTC)",
-		},
-		{
-			type: "body1",
-			text: "- How to use tax-free money for supplies?",
-		},
-		{
-			type: "body1",
-			text: "- How to use unclaimed to grow quickly?",
-		},
-		{
-			type: "body1",
-			text: "- How to build your legions for optimal ROI?",
-		},
-    {
-      type: "space",
-    },
-		{
-			type: "subtitle1",
-			text: "Session 5: Maximising ROI (13 July - 5pm UTC)",
-		},
-		{
-			type: "body1",
-			text: "- Which warriors/beasts is it smart to execute?",
-		},
-		{
-			type: "body1",
-			text: "- What is the perfect AP for legions?",
-		},
-		{
-			type: "body1",
-			text: "- Which legions should you never update?",
-		},
-    {
-      type: "space",
+      type: "h4",
+      text: "Legions University Curriculum",
     },
     {
       type: "space",
     },
-		{
-			type: "h5",
-			text: "The Basics - Full Game Instructions:",
-		},
+    {
+      type: "h5",
+      text: "Session 1: Summoning",
+    },
+    {
+      type: "h6",
+      text: "1.1 How to summon smartly?",
+    },
+    {
+      type: "youtu",
+      link: "https://youtu.be/jOqAfG_ahnk",
+    },
+    {
+      type: "h6",
+      text: "1.2 How to create legions quickly?",
+    },
+    {
+      type: "youtu",
+      link: "https://youtu.be/V6V1KiLMpds",
+    },
+    {
+      type: "h6",
+      text: "1.3 How to optimise your Attack Power?",
+    },
+    {
+      type: "youtu",
+      link: "https://youtu.be/2xHzC7CkKIY",
+    },
     {
       type: "space",
     },
-		{
+    {
+      type: "subtitle1",
+      text: "Session 2: Hunting (22 June - 5pm UTC)",
+    },
+    {
       type: "body1",
-      text: "Before you attend the Legions University sessions, please watch the video below with the full game instructions and read the entire <a class='action' href='https://docs.cryptolegions.app/'>whitepaper</a>. Basics that are covered in the whitepaper or the video below, will not be covered. The tips & tricks taught at Legions University are only for serious players who have graduated and understand the basics of the game.",
+      text: "- Is hunting stronger monsters better?",
+    },
+    {
+      type: "body1",
+      text: "- How to hunt monsters faster?",
+    },
+    {
+      type: "body1",
+      text: "- How to optimise your hunt timing?",
     },
     {
       type: "space",
     },
-	]
-}
+    {
+      type: "subtitle1",
+      text: "Session 3: Claiming (29 June - 5pm UTC)",
+    },
+    {
+      type: "body1",
+      text: "- What is the optimal claiming time?",
+    },
+    {
+      type: "body1",
+      text: "- How to speed up tax decrease?",
+    },
+    {
+      type: "body1",
+      text: "- How does reinvesting earnings impact ROI?",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "subtitle1",
+      text: "Session 4: Using Unclaimed (6 July - 5pm UTC)",
+    },
+    {
+      type: "body1",
+      text: "- How to use tax-free money for supplies?",
+    },
+    {
+      type: "body1",
+      text: "- How to use unclaimed to grow quickly?",
+    },
+    {
+      type: "body1",
+      text: "- How to build your legions for optimal ROI?",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "subtitle1",
+      text: "Session 5: Maximising ROI (13 July - 5pm UTC)",
+    },
+    {
+      type: "body1",
+      text: "- Which warriors/beasts is it smart to execute?",
+    },
+    {
+      type: "body1",
+      text: "- What is the perfect AP for legions?",
+    },
+    {
+      type: "body1",
+      text: "- Which legions should you never update?",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "h4",
+      text: "The Basics - Full Game Instructions:",
+    },
+    {
+      type: "space",
+    },
+    {
+      type: "body1",
+      text: "Before you attend the Legions University sessions, please watch the video below with the full game instructions and read the entire <a class='action' href='https://docs.cryptolegions.app/' target='_blank'>whitepaper</a>. Basics that are covered in the whitepaper or the video below, will not be covered. The tips & tricks taught at Legions University are only for serious players who have graduated and understand the basics of the game.",
+    },
+    {
+      type: "space",
+    },
+  ],
+};
--- src/containers/Tips/Tips.tsx
+++ src/containers/Tips/Tips.tsx
@@ -23,9 +23,12 @@ const Tips = () => {
       <Grid container spacing={2}>
         <Grid item xs={12} md={11} lg={10} sx={{ margin: "auto" }}>
           <Typography variant="h3" sx={{ marginBottom: 2 }}>
-            Tips &amp; Tricks
+            Legions University
           </Typography>
-          <Paper sx={{ padding: [3, 4], backgroundColor: '#16161699' }} elevation={3}>
+          <Paper
+            sx={{ padding: [3, 4], backgroundColor: "#16161699" }}
+            elevation={3}
+          >
             {tipsConstant.content.map((content: any, index: number) => {
               let view = (
                 <Typography
@@ -42,42 +45,86 @@ const Tips = () => {
                 view = (
                   <Box dangerouslySetInnerHTML={{ __html: content.text }}></Box>
                 );
+              } else if (content.type === "youtu") {
+                view = (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: 2,
+                    }}
+                  >
+                    <Grid spacing={2} container>
+                      <Grid item xs={1}></Grid>
+                      <Grid item xs={10}>
+                        <Box
+                          sx={{
+                            width: "100%",
+                            paddingTop: "56.25%",
+                            position: "relative",
+                          }}
+                        >
+                          <Box
+                            sx={{
+                              position: "absolute",
+                              top: 0,
+                              right: 0,
+                              bottom: 0,
+                              left: 0,
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <ReactPlayer
+                              url={content.link}
+                              width="100%"
+                              height="100%"
+                            />
+                          </Box>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={1}></Grid>
+                    </Grid>
+                  </Box>
+                );
               }
               return <Box key={"INDEX_CONTENT_POLICY_" + index}>{view}</Box>;
             })}
-						<Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
-							<Grid spacing={2} container>
-								<Grid item xs={1}></Grid>
-								<Grid item xs={10}>
-									<Box
-										sx={{
-											width: "100%",
-											paddingTop: "56.25%",
-											position: "relative",
-										}}
-									>
-										<Box
-											sx={{
-												position: "absolute",
-												top: 0,
-												right: 0,
-												bottom: 0,
-												left: 0,
-												display: "flex",
-												alignItems: "center",
-											}}
-										>
-											<ReactPlayer
-												url="https://youtu.be/Ujn7WhSymXE"
-												width="100%"
-												height="100%"
-											/>
-										</Box>
-									</Box>
-								</Grid>
-								<Grid item xs={1}></Grid>
-							</Grid>
-						</Box>
+            <Box
+              sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}
+            >
+              <Grid spacing={2} container>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={10}>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      paddingTop: "56.25%",
+                      position: "relative",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0,
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <ReactPlayer
+                        url="https://youtu.be/Ujn7WhSymXE"
+                        width="100%"
+                        height="100%"
+                      />
+                    </Box>
+                  </Box>
+                </Grid>
+                <Grid item xs={1}></Grid>
+              </Grid>
+            </Box>
           </Paper>
         </Grid>
       </Grid>
