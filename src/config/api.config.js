d5c80aa635bcbf32c1672581ffcba1807ee9ee3d
soladity <thomasfullstack@gmail.com>
2022-11-13 22:21:19 -0500

    update: api url

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,7 +1,6 @@
 export const apiConfig = {
-  devServer:
-    "https://cryptolegionsbackend.dev.cryptogames.agency/cryptolegions",
-  proServer: "https://cryptolegionsbackend.cryptogames.agency/cryptolegions",
+  devServer: "https://cryptolegionsbackend.dev.cryptogames.agency",
+  proServer: "https://cryptolegionsbackend.cryptogames.agency",
   subgraphServer:
     "https://api.thegraph.com/subgraphs/name/feloniousgru-super/clv3",
 };
